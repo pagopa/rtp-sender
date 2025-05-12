662264f618d4279c92d263ad2d65fc4052a1c3dd
Luca Consalvi <luca.consalvi@pagopa.it>
2025-05-12 10:04:53 +0200

    Fix to formatting and trigger a release.

--- src/main/java/it/gov/pagopa/rtp/sender/service/oauth/Oauth2TokenService.java
+++ src/main/java/it/gov/pagopa/rtp/sender/service/oauth/Oauth2TokenService.java
@@ -3,5 +3,6 @@ package it.gov.pagopa.rtp.sender.service.oauth;
 import reactor.core.publisher.Mono;
 
 public interface Oauth2TokenService {
-    Mono<String> getAccessToken(String tokenUri, String clientId, String clientSecret, String scope, boolean isMtlsEnabled);
+  Mono<String> getAccessToken(String tokenUri, String clientId, String clientSecret, String scope,
+      boolean isMtlsEnabled);
 }
