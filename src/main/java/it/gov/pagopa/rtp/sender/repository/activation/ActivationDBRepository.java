98224a38a760842fd497f6ac1ceb1afcf6f78a63
Luca Consalvi <luca.consalvi@pagopa.it>
2025-05-12 11:30:28 +0200

    feat: formatting and trigger a release.

--- src/main/java/it/gov/pagopa/rtp/sender/repository/activation/ActivationDBRepository.java
+++ src/main/java/it/gov/pagopa/rtp/sender/repository/activation/ActivationDBRepository.java
@@ -1,6 +1,5 @@
 package it.gov.pagopa.rtp.sender.repository.activation;
 
-
 import org.springframework.stereotype.Repository;
 
 import it.gov.pagopa.rtp.sender.domain.payer.Payer;
@@ -10,24 +9,24 @@ import reactor.core.publisher.Mono;
 @Repository
 public class ActivationDBRepository implements PayerRepository {
 
-    private final ActivationDB activationDB;
-    private final ActivationMapper activationMapper;
+  private final ActivationDB activationDB;
+  private final ActivationMapper activationMapper;
 
-    public ActivationDBRepository(ActivationDB activationDB,
-            ActivationMapper activationMapper) {
-        this.activationDB = activationDB;
-        this.activationMapper = activationMapper;
-    }
+  public ActivationDBRepository(ActivationDB activationDB,
+      ActivationMapper activationMapper) {
+    this.activationDB = activationDB;
+    this.activationMapper = activationMapper;
+  }
 
-    @Override
-    public Mono<Payer> findByFiscalCode(String fiscalCode) {
-        return activationDB.findByFiscalCode(fiscalCode)
-                .map(activationMapper::toDomain);
-    }
+  @Override
+  public Mono<Payer> findByFiscalCode(String fiscalCode) {
+    return activationDB.findByFiscalCode(fiscalCode)
+        .map(activationMapper::toDomain);
+  }
 
-    @Override
-    public Mono<Payer> save(Payer payer) {
-        return activationDB.save(activationMapper.toDbEntity(payer)).map(activationMapper::toDomain);
-    }
+  @Override
+  public Mono<Payer> save(Payer payer) {
+    return activationDB.save(activationMapper.toDbEntity(payer)).map(activationMapper::toDomain);
+  }
 
 }
