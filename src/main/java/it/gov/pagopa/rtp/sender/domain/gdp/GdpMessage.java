bd7e743648a2d91b9586baf8d28637fc8b95e3ba
Francesco Muscianisi <francesco.muscianisi@digitouch.it>
2025-06-03 11:25:49 +0200

    feat: [SRTP-544] add GdpMessage DTO

---
+++ src/main/java/it/gov/pagopa/rtp/sender/domain/gdp/GdpMessage.java
@@ -0,0 +1,40 @@
+package it.gov.pagopa.rtp.sender.domain.gdp;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import java.time.LocalDate;
+import java.util.UUID;
+import org.springframework.validation.annotation.Validated;
+
+
+@Validated
+public record GdpMessage(
+    @NotBlank UUID id,
+    @NotNull Operation operation,
+    @NotNull Long timestamp,
+    String iuv,
+    String subject,
+    String description,
+    String ecTaxCode,
+    String debtorTaxCode,
+    String nav,
+    LocalDate dueDate,
+    @Positive int amount,
+    Status status,
+    String pspCode,
+    String pspTaxCode
+) {
+
+  public enum Operation {
+    CREATE,
+    UPDATE,
+    DELETE
+  }
+
+  public enum Status {
+    VALID,
+    INVALID
+  }
+
+}
