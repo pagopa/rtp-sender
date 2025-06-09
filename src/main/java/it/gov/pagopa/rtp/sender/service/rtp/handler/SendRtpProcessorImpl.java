ee17025adc8c27b7f00e795404e10af7758fd9c9
Michele Fucito <michele.fucito@digitouch.it>
2025-06-09 10:14:59 +0200

    fix [SRTP-563]: typo

--- src/main/java/it/gov/pagopa/rtp/sender/service/rtp/handler/SendRtpProcessorImpl.java
+++ src/main/java/it/gov/pagopa/rtp/sender/service/rtp/handler/SendRtpProcessorImpl.java
@@ -127,7 +127,7 @@ public class SendRtpProcessorImpl implements SendRtpProcessor {
           MDC.put("debtor_service_provider", rtpToSend.serviceProviderDebtor());
           MDC.put("creditor_service_provider", rtpToSend.serviceProviderCreditor());
           MDC.put("payee_name", rtpToSend.payeeName());
-          log.info("Sending RTP to service provider debtor: {}", rtpToSend.serviceProviderDebtor());
+          log.info("Cancelling RTP to service provider debtor: {}", rtpToSend.serviceProviderDebtor());
         })
         .doOnNext(rtp -> log.debug("Creating EPC request for cancellation."))
         .map(EpcRequest::of)
