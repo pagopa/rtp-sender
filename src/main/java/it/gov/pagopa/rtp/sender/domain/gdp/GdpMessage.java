6dabfdcaa3a9c79ea859c87987b5f6b19b7bc0cf
Francesco Muscianisi <francesco.muscianisi@digitouch.it>
2025-06-10 17:17:02 +0200

    chore: [SRTP-543] remove unused imports

--- src/main/java/it/gov/pagopa/rtp/sender/domain/gdp/GdpMessage.java
+++ src/main/java/it/gov/pagopa/rtp/sender/domain/gdp/GdpMessage.java
@@ -1,10 +1,8 @@
 package it.gov.pagopa.rtp.sender.domain.gdp;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import java.time.LocalDate;
-import java.util.UUID;
 import org.springframework.validation.annotation.Validated;
 
 
