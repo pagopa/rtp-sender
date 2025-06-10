9dff15e40596d8fa6bb2db8f0a3bba30ae29dc88
Francesco Muscianisi <francesco.muscianisi@digitouch.it>
2025-06-10 10:42:11 +0200

    chore: [SRTP-543] remove no more needed "spring-cloud-azure-starter-stream-eventhubs" dependency

--- src/main/java/it/gov/pagopa/rtp/sender/domain/gdp/GdpEventHandler.java
+++ src/main/java/it/gov/pagopa/rtp/sender/domain/gdp/GdpEventHandler.java
@@ -1,13 +1,11 @@
 package it.gov.pagopa.rtp.sender.domain.gdp;
 
-import com.azure.spring.messaging.AzureHeaders;
-import com.azure.spring.messaging.checkpoint.Checkpointer;
-import com.azure.spring.messaging.eventhubs.support.EventHubsHeaders;
 import java.util.function.Function;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.lang.NonNull;
 import org.springframework.messaging.Message;
 import reactor.core.publisher.Flux;
@@ -24,21 +22,13 @@ public class GdpEventHandler {
   public Function<Flux<Message<GdpMessage>>, Mono<Void>> gdpMessageConsumer() {
     return gdpMessage -> gdpMessage
         .doOnNext(message -> log.info(
-            "New GDP message received: '{}', partition key: {}, sequence number: {}, offset: {}, enqueued time: {}",
+            "New GDP message received: '{}', partition: {}, offset: {}, enqueued time: {}",
             message.getPayload(),
-            message.getHeaders().get(AzureHeaders.PARTITION_KEY),
-            message.getHeaders().get(EventHubsHeaders.SEQUENCE_NUMBER),
-            message.getHeaders().get(EventHubsHeaders.OFFSET),
-            message.getHeaders().get(EventHubsHeaders.ENQUEUED_TIME)
+            message.getHeaders().get(KafkaHeaders.PARTITION),
+            message.getHeaders().get(KafkaHeaders.OFFSET),
+            message.getHeaders().get(KafkaHeaders.TIMESTAMP)
         ))
         .doOnNext(message -> log.info("Payload: {}", message.getPayload()))
-
-        .map(Message::getHeaders)
-        .mapNotNull(headers -> headers.get(AzureHeaders.CHECKPOINTER))
-        .map(Checkpointer.class::cast)
-        .flatMap(Checkpointer::success)
-
-        .doOnEach(success -> log.info("Message successfully checkpointed"))
         .doOnError(error -> log.error("Exception found", error))
         .then();
   }
