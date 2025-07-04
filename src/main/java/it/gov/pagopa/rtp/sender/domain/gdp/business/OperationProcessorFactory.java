fbce759fae52704273680947744d01d90c8e8b4f
Francesco Muscianisi <francesco.muscianisi@digitouch.it>
2025-07-04 17:44:28 +0200

    feat: [SRTP-631] update OperationProcessorFactory to create UpdatePaidOperationProcessor instances

--- src/main/java/it/gov/pagopa/rtp/sender/domain/gdp/business/OperationProcessorFactory.java
+++ src/main/java/it/gov/pagopa/rtp/sender/domain/gdp/business/OperationProcessorFactory.java
@@ -4,6 +4,8 @@ import it.gov.pagopa.rtp.sender.configuration.GdpEventHubProperties;
 import it.gov.pagopa.rtp.sender.domain.gdp.GdpMapper;
 import it.gov.pagopa.rtp.sender.domain.gdp.GdpMessage;
 import it.gov.pagopa.rtp.sender.domain.gdp.GdpMessage.Operation;
+import it.gov.pagopa.rtp.sender.domain.gdp.GdpMessage.Status;
+import it.gov.pagopa.rtp.sender.service.registryfile.RegistryDataService;
 import it.gov.pagopa.rtp.sender.service.rtp.SendRTPService;
 import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
@@ -32,6 +34,7 @@ public class OperationProcessorFactory {
   private final GdpMapper gdpMapper;
   private final SendRTPService sendRTPService;
   private final GdpEventHubProperties gdpEventHubProperties;
+  private final RegistryDataService registryDataService;
 
 
   /**
@@ -40,23 +43,24 @@ public class OperationProcessorFactory {
    * @param gdpMapper       the GDP-to-RTP mapper; must not be {@code null}
    * @param sendRTPService  the RTP sending service; must not be {@code null}
    * @param gdpEventHubProperties the configuration properties for GDP Event Hub; must not be {@code null}
+   * @param registryDataService the Registry Data Service; must not be {@code null}
    * @throws NullPointerException if any argument is {@code null}
    */
   public OperationProcessorFactory(
           @NonNull final GdpMapper gdpMapper,
           @NonNull final SendRTPService sendRTPService,
-          @NonNull final GdpEventHubProperties gdpEventHubProperties) {
+          @NonNull final GdpEventHubProperties gdpEventHubProperties,
+          @NonNull final RegistryDataService registryDataService) {
 
     this.gdpMapper = Objects.requireNonNull(gdpMapper);
     this.sendRTPService = Objects.requireNonNull(sendRTPService);
     this.gdpEventHubProperties = Objects.requireNonNull(gdpEventHubProperties);
+    this.registryDataService = Objects.requireNonNull(registryDataService);
   }
 
 
   /**
    * Retrieves an appropriate {@link OperationProcessor} based on the {@link Operation} of the given {@link GdpMessage}.
-   *
-   * <p>If the operation is not supported, a {@link UnsupportedOperationException} is returned via {@link Mono#error}.</p>
    *
    * @param gdpMessage the GDP message from which to extract the operation; must not be {@code null}
    * @return a {@link Mono} emitting the resolved {@link OperationProcessor}
@@ -68,32 +72,46 @@ public class OperationProcessorFactory {
 
     return Mono.just(gdpMessage)
         .doFirst(() -> log.debug("Creating processor instance for operation {}", gdpMessage.operation()))
-        .map(GdpMessage::operation)
-        .mapNotNull(this::createProcessorInstance)
-        .switchIfEmpty(
-            Mono.error(new UnsupportedOperationException(gdpMessage.operation().toString())))
+        .map(this::createProcessorInstance)
         .doOnSuccess(processor -> log.debug("Created processor instance for operation {}", gdpMessage.operation()))
         .doOnError(error -> log.error("Error creating processor instance for operation {}", gdpMessage.operation(), error));
   }
 
 
   /**
-   * Internal method to instantiate the {@link OperationProcessor} for a given {@link Operation}.
+   * Internal method to instantiate the {@link OperationProcessor} for given {@link Operation} and {@link Status}.
    *
-   * @param operation the GDP operation to process; must not be {@code null}
+   * @param gdpMessage the GDP message from which to extract the operation; must not be {@code null}
    * @return the corresponding {@link OperationProcessor} instance
    * @throws UnsupportedOperationException if the operation is unsupported
    * @throws NullPointerException if {@code operation} is {@code null}
    */
   @NonNull
-  private OperationProcessor createProcessorInstance(@NonNull final Operation operation) {
-    Objects.requireNonNull(operation, "Operation cannot be null");
+  private OperationProcessor createProcessorInstance(@NonNull final GdpMessage gdpMessage) {
+    Objects.requireNonNull(gdpMessage, "Gdp Message cannot be null");
+    Objects.requireNonNull(gdpMessage.operation(), "Operation cannot be null");
 
-    return switch (operation) {
+    return switch (gdpMessage.operation()) {
       case CREATE -> new CreateOperationProcessor(this.gdpMapper, this.sendRTPService);
-      case UPDATE -> throw new UnsupportedOperationException(operation.toString());
+      case UPDATE -> this.createUpdateProcessorInstance(gdpMessage);
       case DELETE -> new DeleteOperationProcessor(this.sendRTPService, this.gdpEventHubProperties);
     };
+  }
+
+
+  @NonNull
+  private OperationProcessor createUpdateProcessorInstance(@NonNull final GdpMessage gdpMessage) {
+    Objects.requireNonNull(gdpMessage, "Gdp Message cannot be null");
+    Objects.requireNonNull(gdpMessage.operation(), "Operation cannot be null");
+    Objects.requireNonNull(gdpMessage.status(), "Status cannot be null");
+
+    if (gdpMessage.status() == Status.PAID) {
+      return new UpdatePaidOperationProcessor(this.registryDataService, this.sendRTPService, this.gdpEventHubProperties);
+
+    } else {
+      throw new UnsupportedOperationException(
+          String.format("%s %s", gdpMessage.operation(), gdpMessage.status()));
+    }
   }
 
 }
