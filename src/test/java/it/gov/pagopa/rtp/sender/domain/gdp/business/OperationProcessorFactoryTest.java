09c549830679643dac7fd2aa9a81359eb01e92f5
Francesco Muscianisi <francesco.muscianisi@digitouch.it>
2025-07-04 17:45:12 +0200

    test: [SRTP-631] update and improve tests in OperationProcessorFactoryTest

--- src/test/java/it/gov/pagopa/rtp/sender/domain/gdp/business/OperationProcessorFactoryTest.java
+++ src/test/java/it/gov/pagopa/rtp/sender/domain/gdp/business/OperationProcessorFactoryTest.java
@@ -7,12 +7,16 @@ import it.gov.pagopa.rtp.sender.configuration.GdpEventHubProperties;
 import it.gov.pagopa.rtp.sender.domain.gdp.GdpMapper;
 import it.gov.pagopa.rtp.sender.domain.gdp.GdpMessage;
 import it.gov.pagopa.rtp.sender.domain.gdp.GdpMessage.Operation;
+import it.gov.pagopa.rtp.sender.domain.gdp.GdpMessage.Status;
+import it.gov.pagopa.rtp.sender.service.registryfile.RegistryDataService;
 import it.gov.pagopa.rtp.sender.service.rtp.SendRTPService;
-import org.junit.jupiter.api.BeforeEach;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import reactor.test.StepVerifier;
@@ -29,13 +33,11 @@ class OperationProcessorFactoryTest {
   @Mock
   private GdpEventHubProperties gdpEventHubProperties;
 
+  @Mock
+  private RegistryDataService registryDataService;
+
+  @InjectMocks
   private OperationProcessorFactory factory;
-
-
-  @BeforeEach
-  void setUp() {
-    factory = new OperationProcessorFactory(gdpMapper, sendRTPService, gdpEventHubProperties);
-  }
 
 
   @Test
@@ -46,39 +48,78 @@ class OperationProcessorFactoryTest {
   }
 
   @ParameterizedTest
-  @EnumSource(value = Operation.class, names = {"CREATE", "DELETE"})
-  void givenMessageWithSupportedOperation_whenGetProcessor_thenReturnsProcessorInstance(final Operation operation) {
+  @MethodSource("provideSupportedOperationsAndStatuses")
+  void givenMessageWithSupportedOperation_whenGetProcessor_thenReturnsProcessorInstance(
+      Operation operation, Status status, Class<? extends OperationProcessor> expectedProcessorClass) {
     final var message = GdpMessage.builder()
         .operation(operation)
+        .status(status)
         .build();
 
     final var result = factory.getProcessor(message);
 
     StepVerifier.create(result)
-            .assertNext(processor -> {
-              if (operation == Operation.CREATE) {
-                assertThat(processor).isInstanceOf(CreateOperationProcessor.class);
-              } else if (operation == Operation.DELETE) {
-                assertThat(processor).isInstanceOf(DeleteOperationProcessor.class);
-              }
-            })
-            .verifyComplete();
+        .assertNext(processor -> {
+          assertThat(processor).isNotNull();
+          assertThat(processor.getClass())
+              .isEqualTo(expectedProcessorClass);
+        })
+        .verifyComplete();
   }
 
   @ParameterizedTest
-  @EnumSource(value = Operation.class, names = "UPDATE")
-  void givenMessageWithUnsupportedOperation_whenGetProcessor_thenThrowsUnsupportedOperationException(final Operation unsupportedOperation) {
+  @MethodSource("provideUnsupportedOperationsAndStatuses")
+  void givenMessageWithUnsupportedOperation_whenGetProcessor_thenThrowsUnsupportedOperationException(Operation unsupportedOperation, Status status) {
     final var message = GdpMessage.builder()
         .operation(unsupportedOperation)
+        .status(status)
         .build();
+
+    final var expectedErrorMessage = unsupportedOperation.name() + " " + status.name();
 
     final var result = factory.getProcessor(message);
 
     StepVerifier.create(result)
         .expectErrorSatisfies(ex -> {
           assertThat(ex).isInstanceOf(UnsupportedOperationException.class);
-          assertThat(ex).hasMessage(unsupportedOperation.name());
+          assertThat(ex).hasMessage(expectedErrorMessage);
         })
         .verify();
   }
+
+// ---------- PROVIDERS ----------
+
+  private static Stream<Arguments> provideSupportedOperationsAndStatuses() {
+    return Stream.of(
+        Arguments.of(Operation.CREATE, Status.VALID, CreateOperationProcessor.class),
+        Arguments.of(Operation.CREATE, Status.INVALID, CreateOperationProcessor.class),
+        Arguments.of(Operation.CREATE, Status.PARTIALLY_VALID, CreateOperationProcessor.class),
+        Arguments.of(Operation.CREATE, Status.PAID, CreateOperationProcessor.class),
+        Arguments.of(Operation.CREATE, Status.PUBLISHED, CreateOperationProcessor.class),
+        Arguments.of(Operation.CREATE, Status.EXPIRED, CreateOperationProcessor.class),
+        Arguments.of(Operation.CREATE, Status.DRAFT, CreateOperationProcessor.class),
+
+        Arguments.of(Operation.DELETE, Status.VALID, DeleteOperationProcessor.class),
+        Arguments.of(Operation.DELETE, Status.INVALID, DeleteOperationProcessor.class),
+        Arguments.of(Operation.DELETE, Status.PARTIALLY_VALID, DeleteOperationProcessor.class),
+        Arguments.of(Operation.DELETE, Status.PAID, DeleteOperationProcessor.class),
+        Arguments.of(Operation.DELETE, Status.PUBLISHED, DeleteOperationProcessor.class),
+        Arguments.of(Operation.DELETE, Status.EXPIRED, DeleteOperationProcessor.class),
+        Arguments.of(Operation.DELETE, Status.DRAFT, DeleteOperationProcessor.class),
+
+        Arguments.of(Operation.UPDATE, Status.PAID, UpdatePaidOperationProcessor.class)
+    );
+  }
+
+  private static Stream<Arguments> provideUnsupportedOperationsAndStatuses() {
+    return Stream.of(
+        Arguments.of(Operation.UPDATE, Status.VALID),
+        Arguments.of(Operation.UPDATE, Status.INVALID),
+        Arguments.of(Operation.UPDATE, Status.PARTIALLY_VALID),
+        Arguments.of(Operation.UPDATE, Status.PUBLISHED),
+        Arguments.of(Operation.UPDATE, Status.EXPIRED),
+        Arguments.of(Operation.UPDATE, Status.DRAFT)
+    );
+  }
+
 }
