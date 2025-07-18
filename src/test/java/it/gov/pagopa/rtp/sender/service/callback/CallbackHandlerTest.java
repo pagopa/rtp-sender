19459386bc17dd0d9561c3f40ad17c453ca0e7af
Davide Ragusi <davideragusi@192.168.128.113>
2025-07-18 11:44:29 +0200

    fix: [SRTP-697] add missing import

--- src/test/java/it/gov/pagopa/rtp/sender/service/callback/CallbackHandlerTest.java
+++ src/test/java/it/gov/pagopa/rtp/sender/service/callback/CallbackHandlerTest.java
@@ -5,6 +5,8 @@ import it.gov.pagopa.rtp.sender.domain.rtp.*;
 import it.gov.pagopa.rtp.sender.service.rtp.RtpStatusUpdater;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
