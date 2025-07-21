fdfce4d0bd2e13bb46e333814bc6c1cb11c7b287
Francesco Muscianisi <francesco.muscianisi@digitouch.it>
2025-07-21 10:01:13 +0200

    refactor: [SRTP-716] centralize common tracing logic in ReactiveMongoTraceInterceptor

--- src/main/java/it/gov/pagopa/rtp/sender/telemetry/ReactiveMongoTraceInterceptor.java
+++ src/main/java/it/gov/pagopa/rtp/sender/telemetry/ReactiveMongoTraceInterceptor.java
@@ -5,12 +5,16 @@ import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.api.trace.StatusCode;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.Context;
+import java.util.Objects;
+import java.util.Optional;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -25,9 +29,10 @@ import org.springframework.core.annotation.AnnotationUtils;
  */
 public class ReactiveMongoTraceInterceptor implements MethodInterceptor {
 
+  private static final List<String> METHODS_TO_IGNORE = List.of("getMongoTemplate", "getMongoDB");
+
   private final Tracer tracer;
   private final ReactiveMongoTemplate mongoTemplate;
-  private final List<String> methodsToIgnore;
 
   /**
    * Constructs a new ReactiveMongoTraceInterceptor.
@@ -38,7 +43,6 @@ public class ReactiveMongoTraceInterceptor implements MethodInterceptor {
   public ReactiveMongoTraceInterceptor(Tracer tracer, ReactiveMongoTemplate mongoTemplate) {
     this.tracer = tracer;
     this.mongoTemplate = mongoTemplate;
-    this.methodsToIgnore = List.of("getMongoTemplate", "getMongoDB");
   }
 
   /**
@@ -109,22 +113,80 @@ public class ReactiveMongoTraceInterceptor implements MethodInterceptor {
    * @throws Throwable if the invocation fails
    */
   @Override
-  public Object invoke(MethodInvocation invocation) throws Throwable {
-    TraceMongo traceMongo = AnnotationUtils.findAnnotation(invocation.getMethod(), TraceMongo.class);
-    if (traceMongo == null) {
-      traceMongo = AnnotationUtils.findAnnotation(invocation.getThis().getClass(), TraceMongo.class);
-    }
-    if (traceMongo != null) {
-      // Apply tracing logic
-      if (invocation.getMethod().getReturnType().equals(Flux.class))
-        return tracingLogicFlux(invocation);
-      else
-        return tracingMonoLogic(invocation);
-    } else {
-      // Skip tracing
-      return invocation.proceed();
-    }
+  public Object invoke(@NonNull final MethodInvocation invocation) throws Throwable {
+    final var traceMongoMethodAnnotation = Optional.of(invocation)
+        .map(MethodInvocation::getMethod)
+        .map(method ->
+            AnnotationUtils.findAnnotation(method, TraceMongo.class));
+
+    final var traceMongoClassAnnotation = Optional.of(invocation)
+        .map(MethodInvocation::getThis)
+        .map(Object::getClass)
+        .map(clazz ->
+            AnnotationUtils.findAnnotation(clazz, TraceMongo.class));
+
+    return Optional.of(invocation)
+        .flatMap( methodInvocation -> traceMongoMethodAnnotation)
+        .or(() -> traceMongoClassAnnotation)
+        .map(annotation -> tracingLogic(invocation))
+        .orElseGet(() -> {
+          try {
+            return invocation.proceed();
+
+          } catch (Throwable e) {
+            throw new RuntimeException(e);
+          }
+        });
   }
+
+
+  @Nullable
+  private Object tracingLogic(@NonNull final MethodInvocation invocation) {
+    // Check if the target is a ReactiveMongoRepository implementation
+    if (!(invocation.getThis() instanceof ReactiveMongoRepository<?, ?>)) {
+      try {
+        return invocation.proceed();
+
+      } catch (Throwable e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+    final var repository = (ReactiveMongoRepository<?, ?>) invocation.getThis();
+    final var method = invocation.getMethod();
+    final var methodName = method.getName();
+
+    if (METHODS_TO_IGNORE.contains(methodName)) {
+      try {
+        return invocation.proceed();
+
+      } catch (Throwable e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+    final var repositoryName = getRepositoryName(repository);
+    final var collectionName = getCollectionName(repositoryName);
+    final var queryDetails = extractQueryDetails(method, invocation.getArguments());
+    final var returnType = invocation.getMethod()
+        .getReturnType();
+
+    final var span = tracer.spanBuilder(repositoryName + "." + methodName)
+        .setParent(Context.current().with(Span.current()))
+        .setSpanKind(SpanKind.CLIENT)
+        .setAttribute("db.system", "mongodb")
+        .setAttribute("db.operation", methodName)
+        .setAttribute("db.mongodb.collection", collectionName)
+        .setAttribute("db.statement", queryDetails)
+        .startSpan();
+
+
+    return Optional.of(returnType)
+        .filter(type -> type.equals(Mono.class))
+        .map(type -> (Object) tracingMonoLogic(invocation, span))
+        .orElseGet(() -> tracingLogicFlux(invocation, span));
+  }
+
 
   /**
    * Implements the core tracing logic for MongoDB operations returning a Mono.
@@ -133,61 +195,32 @@ public class ReactiveMongoTraceInterceptor implements MethodInterceptor {
    *
    * @param invocation The method invocation to be traced
    * @return A Mono containing the result of the operation
-   * @throws Throwable if the operation fails
    */
-  private Object tracingMonoLogic(MethodInvocation invocation) throws Throwable {
-    // Check if the target is a ReactiveMongoRepository implementation
-    if (!(invocation.getThis() instanceof ReactiveMongoRepository<?, ?>)) {
-      return invocation.proceed();
-    }
+  @NonNull
+  private Mono<?> tracingMonoLogic(
+      @NonNull final MethodInvocation invocation,
+      @NonNull final Span span) {
 
-    ReactiveMongoRepository<?, ?> repository = (ReactiveMongoRepository<?, ?>) invocation.getThis();
-    Method method = invocation.getMethod();
-    String methodName = method.getName();
+    Objects.requireNonNull(invocation);
+    Objects.requireNonNull(span);
 
-    if (methodsToIgnore.contains(methodName)) {
-      return invocation.proceed();
-    }
+    return Mono.deferContextual(ctx -> mongoTemplate.getMongoDatabase()
+        .switchIfEmpty(Mono.error(new IllegalStateException("Database not available")))
+        .flatMap(database -> {
+          span.setAttribute("db.name", database.getName());
 
-    String repositoryName = getRepositoryName(repository);
-    String collectionName = getCollectionName(repositoryName);
-    String queryDetails = extractQueryDetails(method, invocation.getArguments());
-
-    return Mono.deferContextual(ctx -> {
-      // Create and configure the OpenTelemetry span
-      Span span = tracer.spanBuilder(repositoryName + "." + methodName)
-          .setParent(Context.current().with(Span.current()))
-          .setSpanKind(SpanKind.CLIENT)
-          .setAttribute("db.system", "mongodb")
-          .setAttribute("db.operation", methodName)
-          .setAttribute("db.mongodb.collection", collectionName)
-          .setAttribute("db.statement", queryDetails)
-          .startSpan();
-
-      // Execute the MongoDB operation with tracing
-      return mongoTemplate.getMongoDatabase()
-          .switchIfEmpty(Mono.error(new IllegalStateException("Database not available")))
-          .flatMap(database -> {
-            span.setAttribute("db.name", database.getName());
-
-            try {
-              return ((Mono<?>) invocation.proceed())
-                  .doOnSubscribe(subscription -> span.addEvent("MongoDB operation started"))
-                  .doOnSuccess(result -> span.addEvent("MongoDB operation completed successfully"))
-                  .doOnError(error -> {
-                    span.recordException(error);
-                    span.setStatus(StatusCode.ERROR,
-                        "MongoDB operation failed: " + error.getMessage());
-                  })
-                  .doFinally(signalType -> span.end());
-            } catch (Throwable e) {
-              span.recordException(e);
-              span.setStatus(StatusCode.ERROR, "MongoDB operation failed: " + e.getMessage());
-              span.end();
-              return Mono.error(e);
-            }
-          });
-    });
+          try {
+            return ((Mono<?>) invocation.proceed())
+                .doOnSubscribe(subscription -> span.addEvent("MongoDB operation started"))
+                .doOnSuccess(result -> span.addEvent("MongoDB operation completed successfully"))
+                .doOnError(error -> this.enrichSpanOnError(span, error))
+                .doFinally(signalType -> span.end());
+          } catch (Throwable e) {
+            this.enrichSpanOnError(span, e);
+            span.end();
+            return Mono.error(e);
+          }
+        }));
   }
 
 
@@ -198,61 +231,47 @@ public class ReactiveMongoTraceInterceptor implements MethodInterceptor {
    *
    * @param invocation The method invocation to be traced
    * @return A Flux containing the result of the operation
-   * @throws Throwable if the operation fails
    */
-  private Object tracingLogicFlux(MethodInvocation invocation) throws Throwable {
-    // Check if the target is a ReactiveMongoRepository implementation
-    if (!(invocation.getThis() instanceof ReactiveMongoRepository<?, ?>)) {
-      return invocation.proceed();
-    }
+  @NonNull
+  private Object tracingLogicFlux(
+      @NonNull final MethodInvocation invocation,
+      @NonNull final Span span) {
 
-    ReactiveMongoRepository<?, ?> repository = (ReactiveMongoRepository<?, ?>) invocation.getThis();
-    Method method = invocation.getMethod();
-    String methodName = method.getName();
+    return Flux.deferContextual(ctx -> mongoTemplate.getMongoDatabase()
+        .switchIfEmpty(Mono.error(new IllegalStateException("Database not available")))
+        .flatMapMany(Flux::just)
+        .flatMap(database -> {
+          span.setAttribute("db.name", database.getName());
 
-    if (methodsToIgnore.contains(methodName)) {
-      return invocation.proceed();
-    }
+          try {
+            return ((Flux<?>) invocation.proceed())
+                .doOnSubscribe(subscription -> span.addEvent("MongoDB operation started"))
+                .doOnComplete(() -> span.addEvent("MongoDB operation completed successfully"))
+                .doOnError(error -> this.enrichSpanOnError(span, error))
+                .doFinally(signalType -> span.end());
+          } catch (Throwable e) {
+            this.enrichSpanOnError(span, e);
+            span.end();
+            return Flux.error(e);
+          }
+        }));
+  }
 
-    String repositoryName = getRepositoryName(repository);
-    String collectionName = getCollectionName(repositoryName);
-    String queryDetails = extractQueryDetails(method, invocation.getArguments());
 
-    return Flux.deferContextual(ctx -> {
-      // Create and configure the OpenTelemetry span
-      Span span = tracer.spanBuilder(repositoryName + "." + methodName)
-          .setParent(Context.current().with(Span.current()))
-          .setSpanKind(SpanKind.CLIENT)
-          .setAttribute("db.system", "mongodb")
-          .setAttribute("db.operation", methodName)
-          .setAttribute("db.mongodb.collection", collectionName)
-          .setAttribute("db.statement", queryDetails)
-          .startSpan();
+  /**
+   * Enriches a span with error information and status.
+   *
+   * @param span the span to enrich
+   * @param error the error to enrich the span with
+   */
+  private void enrichSpanOnError(
+      @NonNull final Span span,
+      @NonNull final Throwable error) {
 
-      // Execute the MongoDB operation with tracing
-      return mongoTemplate.getMongoDatabase()
-          .switchIfEmpty(Mono.error(new IllegalStateException("Database not available")))
-          .flatMapMany(Flux::just)
-          .flatMap(database -> {
-            span.setAttribute("db.name", database.getName());
+    Objects.requireNonNull(span);
+    Objects.requireNonNull(error);
 
-            try {
-              return ((Flux<?>) invocation.proceed())
-                  .doOnSubscribe(subscription -> span.addEvent("MongoDB operation started"))
-                  .doOnComplete(() -> span.addEvent("MongoDB operation completed successfully"))
-                  .doOnError(error -> {
-                    span.recordException(error);
-                    span.setStatus(StatusCode.ERROR,
-                        "MongoDB operation failed: " + error.getMessage());
-                  })
-                  .doFinally(signalType -> span.end());
-            } catch (Throwable e) {
-              span.recordException(e);
-              span.setStatus(StatusCode.ERROR, "MongoDB operation failed: " + e.getMessage());
-              span.end();
-              return Flux.error(e);
-            }
-          });
-    });
+    span.recordException(error);
+    span.setStatus(StatusCode.ERROR, "MongoDB operation failed: " + error.getMessage());
   }
 }
