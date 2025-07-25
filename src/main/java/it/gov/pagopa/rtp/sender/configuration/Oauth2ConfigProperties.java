722e7c042bb6a333668fc0b9ebb9f0588088a407
Francesco Muscianisi <francesco.muscianisi@digitouch.it>
2025-07-25 14:28:37 +0200

    fix: [SRTP-751] use spring security oauth config directly to allow token call to work

--- src/main/java/it/gov/pagopa/rtp/sender/configuration/Oauth2ConfigProperties.java
+++ src/main/java/it/gov/pagopa/rtp/sender/configuration/Oauth2ConfigProperties.java
@@ -4,12 +4,13 @@ import jakarta.validation.constraints.NotBlank;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
-@ConfigurationProperties(prefix = "client.oauth.mil-auth")
+@ConfigurationProperties(prefix = "spring.security.oauth2.client.registration.mil-auth")
 @Validated
 public record Oauth2ConfigProperties (
+    @NotBlank String provider,
     @NotBlank String clientId,
     @NotBlank String clientSecret,
+    @NotBlank String clientAuthenticationMethod,
     @NotBlank String authorizationGrantType,
-    @NotBlank String tokenUri,
-    @NotBlank String provider
+    @NotBlank String clientName
 ) {}
