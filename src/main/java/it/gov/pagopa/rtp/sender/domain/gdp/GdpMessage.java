f8955963b99f9f71718e740e3edae7424dd98c58
Francesco Muscianisi <francesco.muscianisi@digitouch.it>
2025-08-01 11:46:34 +0200

    feat: [SRTP-770] add GdpMessage::nullMessage method

--- src/main/java/it/gov/pagopa/rtp/sender/domain/gdp/GdpMessage.java
+++ src/main/java/it/gov/pagopa/rtp/sender/domain/gdp/GdpMessage.java
@@ -26,6 +26,11 @@ public record GdpMessage(
     String psp_tax_code
 ) {
 
+  public static GdpMessage nullMessage() {
+    return new GdpMessage(0L, Operation.NULL, 0L, "", "", "", "", "", "", null, 0, Status.NULL, "",
+        "");
+  }
+
   public GdpMessage{
     status = Optional.ofNullable(status)
             .orElse(Status.NULL);
@@ -34,7 +39,8 @@ public record GdpMessage(
   public enum Operation {
     CREATE,
     UPDATE,
-    DELETE
+    DELETE,
+    NULL
   }
 
   public enum Status {
