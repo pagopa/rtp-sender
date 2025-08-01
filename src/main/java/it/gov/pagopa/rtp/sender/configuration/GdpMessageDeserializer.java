d621fefe043547298b5a2befbfe41e9eadd5be67
Francesco Muscianisi <francesco.muscianisi@digitouch.it>
2025-08-01 11:09:02 +0200

    refactor: [SRTP-770] reshape GdpMessageDeserializer in a more functional-style

--- src/main/java/it/gov/pagopa/rtp/sender/configuration/GdpMessageDeserializer.java
+++ src/main/java/it/gov/pagopa/rtp/sender/configuration/GdpMessageDeserializer.java
@@ -1,36 +1,37 @@
 package it.gov.pagopa.rtp.sender.configuration;
 
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.gov.pagopa.rtp.sender.domain.gdp.GdpMessage;
+import java.io.IOException;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.Deserializer;
+import org.springframework.stereotype.Component;
 
-import java.util.Map;
 
+@Component("gdpMessageDeserializer")
 @Slf4j
 public class GdpMessageDeserializer implements Deserializer<GdpMessage> {
 
   private final ObjectMapper objectMapper = new ObjectMapper();
 
-  @Override
-  public void configure(Map<String, ?> configs, boolean isKey) {
-    // No configuration needed
-  }
 
   @Override
   public GdpMessage deserialize(String topic, byte[] data) {
-    try {
-      return objectMapper.readValue(data, GdpMessage.class);
-    } catch (Exception e) {
-      log.warn("Discarded invalid GdpMessage from topic [{}]: {}", topic, e.getMessage());
-      log.debug("Invalid raw payload: {}", new String(data));
-      return null; // Return null to skip the message
-    }
-  }
+    return Optional.ofNullable(data)
+        .map(bytes -> {
+          try {
+            return objectMapper.readValue(data, GdpMessage.class);
 
-  @Override
-  public void close() {
-    // No cleanup necessary
+          } catch (IOException e) {
+            log.warn("Discarded invalid GdpMessage from topic [{}]: {}", topic, e.getMessage());
+            log.debug("Invalid raw payload: {}", new String(data));
+            return null;
+          }
+        })
+        .orElse(null);
   }
 }
 
