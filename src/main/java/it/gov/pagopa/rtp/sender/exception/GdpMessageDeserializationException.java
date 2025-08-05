4be85f0f72c77747bd9ac5321c603c206a8b3824
LarissaASLeite <105739146+LarissaASLeite@users.noreply.github.com>
2025-08-05 16:43:28 +0200

    SRTP-770-add-safe-deserializer-for-gdpmessage

--- src/main/java/it/gov/pagopa/rtp/sender/exception/GdpMessageDeserializationException.java
+++
@@ -1,7 +0,0 @@
-package it.gov.pagopa.rtp.sender.exception;
-
-public class GdpMessageDeserializationException extends RuntimeException {
-  public GdpMessageDeserializationException(String message, Throwable cause) {
-    super(message, cause);
-  }
-}
