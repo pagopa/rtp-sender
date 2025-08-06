c4f5ac448e822999c89955e7cf71dcf01843586a
Michele Fucito <michele.fucito@digitouch.it>
2025-08-06 09:05:06 +0200

    fix [SRTP-770]: add native json dependecies

--- src/main/java/it/gov/pagopa/rtp/sender/configuration/KafkaErrorHandlingConfiguration.java
+++ src/main/java/it/gov/pagopa/rtp/sender/configuration/KafkaErrorHandlingConfiguration.java
@@ -1,6 +1,5 @@
 package it.gov.pagopa.rtp.sender.configuration;
 
-import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.listener.CommonErrorHandler;
@@ -10,13 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @Slf4j
-@RegisterReflectionForBinding({
-    org.springframework.messaging.converter.MessageConversionException.class,
-    org.springframework.kafka.support.serializer.DeserializationException.class,
-    com.fasterxml.jackson.core.JsonParseException.class,
-    com.fasterxml.jackson.databind.exc.InvalidFormatException.class,
-    com.fasterxml.jackson.databind.exc.MismatchedInputException.class
-})
 public class KafkaErrorHandlingConfiguration {
     @Bean
     public CommonErrorHandler kafkaErrorHandler() {
